23274e03a0a94c7e061269c169a4b8be98a980b3
1234567 <10010@qq.com>
2019-08-27 18:16:52 +0800

    zhou

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,7 +10,8 @@
             <div style="display:flex">
               <!-- 轮播内容 -->
               <div style="width:900px;height:300px">
-                <el-carousel width="900px" height="300px" :interval="4000" arrow="never">
+                <el-carousel width="900px" height="300px" :interval="4000"
+                >
                   <el-carousel-item v-for="(item,index) in bannerList" :key="index">
                       <img :src="item" width="900px" height="300px"/>
                   </el-carousel-item>
@@ -248,44 +249,214 @@
            <img src="" style="width:30px;height:30px"/>
          </div>
          <p style="padding: 10px 0;">强健安全体系</p>
-         <p style="padding: 10px 0;">AI 技术及微服务拆分设计应用，</p>
-         <p style="padding: 10px 0;">智能提升交易安全系数</p>
+         <p style="padding: 1px 0;">AI 技术及微服务拆分设计应用，</p>
+         <p style="padding: 1px 0;">智能提升交易安全系数</p>
        </li>
        <li style="text-align: center;">
-         <div>
+         <div style="padding: 10px 0;">
            <img src="" style="width:30px;height:30px"/>
          </div>
-         <p>强健安全体系</p>
-         <p>AI 技术及微服务拆分设计应用，</p>
-         <p>智能提升交易安全系数</p>
+         <p style="padding: 10px 0;">强健安全体系</p>
+         <p style="padding: 1px 0;">AI 技术及微服务拆分设计应用，</p>
+         <p style="padding: 1px 0;">智能提升交易安全系数</p>
        </li>
        <li style="text-align: center;">
-         <div>
+         <div style="padding: 10px 0;">
            <img src="" style="width:30px;height:30px"/>
          </div>
-         <p>强健安全体系</p>
-         <p>AI 技术及微服务拆分设计应用，</p>
-         <p>智能提升交易安全系数</p>
+         <p style="padding: 10px 0;">强健安全体系</p>
+         <p style="padding: 1px 0;">AI 技术及微服务拆分设计应用，</p>
+         <p style="padding: 1px 0;">智能提升交易安全系数</p>
        </li>
        <li style="text-align: center;">
-         <div>
+         <div style="padding: 10px 0;">
            <img src="" style="width:30px;height:30px"/>
          </div>
-         <p>强健安全体系</p>
-         <p>AI 技术及微服务拆分设计应用，</p>
-         <p>智能提升交易安全系数</p>
+         <p style="padding: 10px 0;">强健安全体系</p>
+         <p style="padding: 1px 0;">AI 技术及微服务拆分设计应用，</p>
+         <p style="padding: 1px 0;">智能提升交易安全系数</p>
        </li>
        <li style="text-align: center;">
-         <div>
+         <div style="padding: 10px 0;">
            <img src="" style="width:30px;height:30px"/>
          </div>
-         <p>强健安全体系</p>
-         <p>AI 技术及微服务拆分设计应用，</p>
-         <p>智能提升交易安全系数</p>
+         <p style="padding: 10px 0;">强健安全体系</p>
+         <p style="padding: 1px 0;">AI 技术及微服务拆分设计应用，</p>
+         <p style="padding: 1px 0;">智能提升交易安全系数</p>
        </li>
      </ul>
-
    </div>
+   <!-- 5个导航显示结束 -->
+
+   <!-- bibox官方app下载 -->
+   <div style="background-color: #1d3566;color: #fff;padding-top: 30px;margin-top: 40px;">
+      <div style="width: 1200px;margin: 0 auto;">
+        <p style="text-align: center;padding: 15px 0;">Bibox 官方 App 下载</p>
+
+        <div>
+          <ul style="display: flex;justify-content: center;align-items: center;padding: 15px 0;">
+            <li style="display: flex;align-items: center;padding: 15px 30px;">
+              <img src="" style="width:40px;height:40px"/>
+              <span style="margin-left:15px">Iphone</span>
+            </li>
+            <li style="display: flex;align-items: center;padding: 15px 30px">
+              <img src="" style="width:40px;height:40px"/>
+              <span style="margin-left:15px">Android</span>
+            </li>
+            <li style="display: flex;align-items: center;padding: 15px 30px">
+              <img src="" style="width:40px;height:40px"/>
+              <span style="margin-left:15px">windows</span>
+            </li>
+            <li style="display: flex;align-items: center;padding: 15px 30px">
+              <img src="" style="width:40px;height:40px"/>
+              <span style="margin-left:15px">Mas os</span>
+            </li>
+          </ul>
+        </div>
+          
+          <div style="text-align:center"> 
+            <img src="../assets/img/footter.png" style="width:100%;"/>
+          </div>
+
+      </div>
+   </div>
+    <!-- bibox官方app下载结束 -->
+
+
+    <!-- 底部的帮助中心 -->
+    <div style="background-color: #101929;color: #ffff;padding: 30px 0;">
+       <div style="width: 1200px;margin: 0 auto;">
+          <!-- 帮助列表第一行 -->
+          <div>
+            <ul style="display: flex;justify-content: space-between;">
+              <li class="zh-footer-li">
+                <p>
+                  <span>图标</span>
+                  <span>提交工单</span>
+                </p>
+                <p>
+                  <span>图标</span>
+                  <span>加入我们</span>
+                </p>
+                <p>
+                  <span>图标</span>
+                  <span>费率标准</span>
+                </p>
+              </li>
+              <li class="zh-footer-li">
+                <p>
+                  <span>产品</span>
+                </p>
+                <p>
+                  <span>上币申请</span>
+                </p>
+                <p>
+                  <span>存币宝</span>
+                </p>
+                <p>
+                  <span>Lab</span>
+                </p>
+                <p>
+                  <span>API接入</span>
+                </p>
+                <p>
+                  <span>Bibox通证债券</span>
+                </p>
+              </li>
+              <li class="zh-footer-li">
+                <p>
+                  <span>公告帮助</span>
+                </p>
+                <p>
+                  <span>官方公告</span>
+                </p>
+                <p>
+                  <span>帮助中心</span>
+                </p>
+                <p>
+                  <span>用户协议</span>
+                </p>
+                <p>
+                  <span>隐私政策</span>
+                </p>
+              </li>
+              <li class="zh-footer-li">
+                <p>
+                  <span>联系我们</span>
+                </p>
+                <p>
+                  <span>Telegram-中文</span>
+                </p>
+                <p>
+                  <span>Telegram-English</span>
+                </p>
+                <p>
+                  <span>Telegram-한국어</span>
+                </p>
+                <p>
+                  <span>微信客服: BiboxKf-03</span>
+                </p>
+                 <p>
+                  <span>官方邮箱:service@bibox.zendesk.com</span>
+                </p>
+              </li>
+              <li class="zh-footer-li">
+                <p>
+                  <span>社交媒体</span>
+                </p>
+                <p>
+                  <span>Facebook</span>
+                </p>
+                <p>
+                  <span>Twitter</span>
+                </p>
+                <p>
+                  <span>Reddit</span>
+                </p>
+                <p>
+                  <span>Weibo</span>
+                </p>
+                 <p>
+                  <span>Medium</span>
+                </p>
+                <p>
+                  <span>BiYong</span>
+                </p>
+              </li>
+            </ul>
+          </div>
+           <!-- 第二行 -->
+          <div style="padding: 17px 0;">
+            <span>图标</span>
+            <span style="margin-left:6px">LinkCoin</span>
+            <span style="margin-left:14px">C2C服务由合作伙伴 LinkCoin 提供技术及服务支持</span>
+          </div>
+          
+          <!-- 第三行 -->
+          <div>
+            <span style="margin-right:15px">友情链接</span>
+            <span style="margin-right:15px">LinkCoin</span>
+            <span style="margin-right:15px">CoinPark</span>
+            <span style="margin-right:15px">RatingToken</span>
+            <span style="margin-right:15px">玩币族</span>
+            <span style="margin-right:15px">BITCOIN86</span>
+            <span style="margin-right:15px">陀螺财经</span>
+            <span style="margin-right:15px">链向财经</span>
+            <span style="margin-right:15px">共享财经</span>
+            <span style="margin-right:15px">BITKAN</span>
+            <span style="margin-right:15px">Block123</span>
+            <span style="margin-right:15px">F2Pool鱼池</span>
+            <span style="margin-right:15px">Cobo钱包</span>
+            <span style="margin-right:15px">降维安全实验室</span>
+            <span style="margin-right:15px">链得得</span>
+            <span>CoinGecko</span>
+          </div>
+
+       </div>
+    </div>
+    <!-- 底部的帮助中心结束 -->
+
+
 
    </div>
 </template>
@@ -306,6 +477,54 @@ export default {
       currentList:[],
       tabList:{
         usdtData:[
+          {
+            name1:'BTC/USDT',
+            name2:'10312.89',
+            name3:'-0.81%',
+            name4:'10440.00',
+            name5:'10270.11',     
+            name6:'9825.63'
+          },
+          {
+            name1:'BTC/USDT',
+            name2:'10312.89',
+            name3:'-0.81%',
+            name4:'10440.00',
+            name5:'10270.11',     
+            name6:'9825.63'
+          },
+          {
+            name1:'BTC/USDT',
+            name2:'10312.89',
+            name3:'-0.81%',
+            name4:'10440.00',
+            name5:'10270.11',     
+            name6:'9825.63'
+          },
+          {
+            name1:'BTC/USDT',
+            name2:'10312.89',
+            name3:'-0.81%',
+            name4:'10440.00',
+            name5:'10270.11',     
+            name6:'9825.63'
+          },
+          {
+            name1:'BTC/USDT',
+            name2:'10312.89',
+            name3:'-0.81%',
+            name4:'10440.00',
+            name5:'10270.11',     
+            name6:'9825.63'
+          },
+          {
+            name1:'BTC/USDT',
+            name2:'10312.89',
+            name3:'-0.81%',
+            name4:'10440.00',
+            name5:'10270.11',     
+            name6:'9825.63'
+          },
           {
             name1:'BTC/USDT',
             name2:'10312.89',
@@ -451,4 +670,10 @@ export default {
    text-align: center
 }
 
+.zh-footer-li{
+  padding: 10px;
+}
+.zh-footer-li p{
+  padding: 3px 0;
+}
 </style>
