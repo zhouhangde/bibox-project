58477b4f1cd95b9638f958a6160eeb6cc79d540f
1234567 <10010@qq.com>
2019-08-28 19:31:21 +0800

    zhou

---
+++ src/components/bibi/BibBottom.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="BibBottom">
+
+    <!-- 第三大行（包含当前委托，计划委托，冰山委托，历史委托） -->  
+    <div>
+          <!-- 第一小行 -->
+          <div style="display: flex;justify-content: space-between;padding: 25px 10px 3px;">
+             <div style="width: 462px;justify-content: space-between;display: flex;">
+               <span style="padding: 5px;color: #3b85f5;font-weight: 600;border-bottom: 2px solid #3b85f5;">当前委托</span>
+               <span style="padding: 5px;">计划委托</span>
+               <span style="padding: 5px;">冰山委托</span>
+               <span style="padding: 5px;">历史委托</span>
+             </div>
+             <div style="display: flex;align-items: center;">
+               <input type="checkBox"/>
+               <span style="margin-left:5px">只看当前交易币种记录</span>
+               <span style="color: #3b85f5;font-weight: 600;margin-left:20px">查看更多</span>
+             </div>
+          </div>
+
+          <!-- 第二小行 -->
+          <table class="bzjy_table" border="0" cellpadding="0" cellspacing="0" width="100%" style="padding: 6px;border-top: 1px solid #373535;">
+               <thead>
+                 <tr>
+                    <th>时间</th>
+                    <th>市场</th>
+                    <th>方向</th>
+                    <th>委托类型</th>
+                    <th>委托/成交价格	</th>
+                    <th>委托/成交数量	</th>
+                    <th>委托/成交金额	</th>
+                    <th>操作	</th>
+                 </tr>
+               </thead>
+               <tbody style="border-radius: 4px;overflow: hidden;background-color: #242933;padding: 8px 0;">
+                 <tr>
+                    <td>1111</td>
+                    <td>11111</td>
+                    <td>11111</td>
+                    <td>11111</td>
+                    <td>11111</td>
+                    <td>11111</td>
+                    <td>11111</td>
+                    <td>查看</td>
+                 </tr>
+               </tbody>
+             </table>
+        </div>
+
+  </div>
+</template>
+
+<script>
+// import FilterView from "../components/FilterView";
+export default {
+  name: "BibBottom",
+  data() {
+    return {
+    };
+  },
+  computed: {
+  },
+  created() {
+    this.getData();
+  },
+  methods: {
+    getData() {
+    }
+  },
+  components: {
+  }
+};
+</script>
+
+<style scoped>
+
+.BibBottom {
+  width: 100%;
+  overflow: auto;
+  box-sizing: border-box;
+}
+
+.bzjy_table th{
+  text-align: center;
+  padding: 1px 2px;
+}
+
+.bzjy_table td{
+  text-align: center;
+  padding: 1px 2px;
+}
+
+</style>
---
+++ src/components/bibi/BibContent.vue
@@ -0,0 +1,559 @@
+<template>
+  <div class="BibContent">
+
+    <!-- 第二大行(内部包含3个块)   --> 
+    <div style="display:flex">
+          <!-- 左侧的块 -->
+          <div style="flex-basis: 280px;border-radius: 4px;background-color: #212733;border: 1px solid #212733;">
+             <!-- 左侧第一行 -->
+             <div style="display: flex;padding: 10px;justify-content: space-between;background-color: #2d3442;">
+               <!-- 三个tab切换 -->
+                <div>
+                  <span style="padding:5px;color: #3b85f5;font-weight: 600;border-bottom: 2px solid #3b85f5;" @click="filterLeft(1)">tab1</span>
+                  <span style="padding:5px" @click="filterLeft(2)">tab2</span>
+                  <span style="padding:5px" @click="filterLeft(3)">tab3</span>
+                </div>
+                <div>
+                  <span>合并深度:</span>
+                  <span>4位小数</span>
+                </div>
+             </div>
+             <!-- 左侧第一行结束 -->
+
+             <table class="bzjy_table" border="0" cellpadding="0" cellspacing="0" width="100%" style="padding: 6px;">
+               <thead>
+                 <tr>
+                    <th style="width:110px">价格(USDT)</th>
+                    <th style="width:110px">数量(BTC)</th>
+                    <th style="width:110px">金额(USDT)</th>
+                 </tr>
+               </thead>
+               <tbody>
+                 <tr>
+                    <td>10224</td>
+                    <td>0.2930</td>
+                    <td>2990.3485</td>
+                 </tr>
+                 <tr>
+                    <td>2</td>
+                    <td>0.2930</td>
+                    <td>2990.3485</td>
+                 </tr>
+                 <tr>
+                    <td>3</td>
+                    <td>0.2930</td>
+                    <td>2990.3485</td>
+                 </tr>
+                 <tr>
+                    <td>4</td>
+                    <td>0.2930</td>
+                    <td>2990.3485</td>
+                 </tr>
+                 <tr>
+                    <td>5</td>
+                    <td>0.2930</td>
+                    <td>2990.3485</td>
+                 </tr>
+                 <tr>
+                    <td>6</td>
+                    <td>0.2930</td>
+                    <td>2990.3485</td>
+                 </tr>
+                 <tr>
+                    <td>7</td>
+                    <td>0.2930</td>
+                    <td>2990.3485</td>
+                 </tr>
+                 <tr>
+                    <td>8</td>
+                    <td>0.2930</td>
+                    <td>2990.3485</td>
+                 </tr>
+                 <tr>
+                    <td>9</td>
+                    <td>0.2930</td>
+                    <td>2990.3485</td>
+                 </tr>
+                 <tr>
+                    <td>10</td>
+                    <td>0.2930</td>
+                    <td>2990.3485</td>
+                 </tr>
+                 <tr>
+                    <td>11</td>
+                    <td>0.2930</td>
+                    <td>2990.3485</td>
+                 </tr>
+                 <tr>
+                    <td>12</td>
+                    <td>0.2930</td>
+                    <td>2990.3485</td>
+                 </tr>
+                 <tr>
+                    <td>13</td>
+                    <td>0.2930</td>
+                    <td>2990.3485</td>
+                 </tr>
+                 <tr>
+                    <td>14</td>
+                    <td>0.2930</td>
+                    <td>2990.3485</td>
+                 </tr>
+                 <tr>
+                    <td>15</td>
+                    <td>0.2930</td>
+                    <td>2990.3485</td>
+                 </tr>
+                 <tr>
+                    <td>16</td>
+                    <td>0.2930</td>
+                    <td>2990.3485</td>
+                 </tr>
+               </tbody>
+             </table>
+             
+             <!-- 中间 -->
+             <div style="text-align:center;">
+               10,165.0002 ≈ 72,790.68 CNY
+             </div>
+
+             <table class="bzjy_table" border="0" cellpadding="0" cellspacing="0" width="100%" style="padding: 6px;">
+               <thead>
+                 <tr>
+                    <th style="width:110px"></th>
+                    <th style="width:110px"></th>
+                    <th style="width:110px"></th>
+                 </tr>
+               </thead>
+               <tbody>
+                 <tr>
+                    <td>10224</td>
+                    <td>0.2930</td>
+                    <td>2990.3485</td>
+                 </tr>
+                 <tr>
+                    <td>2</td>
+                    <td>0.2930</td>
+                    <td>2990.3485</td>
+                 </tr>
+                 <tr>
+                    <td>3</td>
+                    <td>0.2930</td>
+                    <td>2990.3485</td>
+                 </tr>
+                 <tr>
+                    <td>4</td>
+                    <td>0.2930</td>
+                    <td>2990.3485</td>
+                 </tr>
+                 <tr>
+                    <td>5</td>
+                    <td>0.2930</td>
+                    <td>2990.3485</td>
+                 </tr>
+                 <tr>
+                    <td>6</td>
+                    <td>0.2930</td>
+                    <td>2990.3485</td>
+                 </tr>
+                 <tr>
+                    <td>7</td>
+                    <td>0.2930</td>
+                    <td>2990.3485</td>
+                 </tr>
+                 <tr>
+                    <td>8</td>
+                    <td>0.2930</td>
+                    <td>2990.3485</td>
+                 </tr>
+                 <tr>
+                    <td>9</td>
+                    <td>0.2930</td>
+                    <td>2990.3485</td>
+                 </tr>
+                 <tr>
+                    <td>10</td>
+                    <td>0.2930</td>
+                    <td>2990.3485</td>
+                 </tr>
+                 <tr>
+                    <td>11</td>
+                    <td>0.2930</td>
+                    <td>2990.3485</td>
+                 </tr>
+                 <tr>
+                    <td>12</td>
+                    <td>0.2930</td>
+                    <td>2990.3485</td>
+                 </tr>
+                 <tr>
+                    <td>13</td>
+                    <td>0.2930</td>
+                    <td>2990.3485</td>
+                 </tr>
+                 <tr>
+                    <td>14</td>
+                    <td>0.2930</td>
+                    <td>2990.3485</td>
+                 </tr>
+                 <tr>
+                    <td>15</td>
+                    <td>0.2930</td>
+                    <td>2990.3485</td>
+                 </tr>
+                 <tr>
+                    <td>16</td>
+                    <td>0.2930</td>
+                    <td>2990.3485</td>
+                 </tr>
+               </tbody>
+             </table>
+
+
+          </div>
+
+          <!-- 中间的块 -->
+          <div style="flex-grow: 1;margin-left:8px;display: flex;flex-direction: column;justify-content: space-between;">
+            <div>
+             <iframe id="" name="" src="" frameborder="0" allowtransparency="true" scrolling="no" allowfullscreen="" style="display: block; width: 100%; height: 420px;border-radius: 4px;background-color: #212733;border: 1px solid #212733;">
+
+	           </iframe>
+            </div>
+
+            <div style="border-radius: 4px;background-color: rgb(33, 39, 51);border: 1px solid rgb(33, 39, 51);padding: 15px 0;">
+              <!-- tab切换 -->
+              <div style="padding:10px 20px 6px">
+                <span style="padding:5px 20px;color: #3b85f5;font-weight: 600;border-bottom: 2px solid #3b85f5;">限价委托</span>
+                <span style="padding:5px 20px">计划委托</span>
+                <span style="padding:5px 20px">冰山委托</span>
+              </div>
+              <div style="display: flex;justify-content: space-between;">
+                <!-- 左侧的块 -->
+                <div style="padding:10px;display: flex;flex-direction: column;flex-grow: 1;border-right: 1px double rgb(45, 52, 66);">
+                  <!-- 第一行 -->
+                  <div style="display: flex;justify-content: space-between;padding: 10px 0;">
+                    <span>买入BTC</span>
+                    <p>
+                      <span>USDT</span>
+                      <span>可用:0.00000000</span>
+                      <span>互转图标</span>
+                    </p>
+                  </div>
+                  <!-- 第二行 -->
+                  <div style="border: 1px solid #000;margin-top: 8px;padding: 5px;background-color: rgb(45, 52, 66);border-radius: 4px;border: 1px solid rgb(45, 52, 66);display: flex;justify-content: space-between;"> 
+                    <span>买价</span>
+                    <input style="width:150px" placeholder="10154.7000"/>
+                    <span>USDT</span>
+                  </div>
+                  <!-- 第三行 -->
+                  <div style="padding:4px 68px">
+                    ≈ 72716.92CNY
+                  </div>
+                  <!-- 第四行 -->
+                  <div style="background-color: rgb(45, 52, 66);border-radius: 4px;border: 1px solid rgb(45, 52, 66);margin-top: 8px;padding: 5px;display: flex;justify-content: space-between;"> 
+                    <span>数量</span>
+                    <input style="width:150px" placeholder="0.0000"/>
+                    <span>BTC</span>
+                  </div>
+
+                  <!-- 第五行 -->
+                  <div style="padding: 2px 37px;display: flex;justify-content: space-between;border: 1px dashed #000;margin-top: 8px;">
+                    <span>25%</span>
+                    <span>50%</span>
+                    <span>75%</span>
+                    <span>100%</span>
+                  </div>
+
+                  <!-- 第六行 -->
+                  <div style="background-color: rgb(45, 52, 66);border-radius: 4px;border: 1px solid rgb(45, 52, 66);margin-top: 8px;padding: 5px;display: flex;justify-content: space-between;"> 
+                    <span>金额</span>
+                    <input style="width:150px" placeholder="10154.7000"/>
+                    <span>USDT</span>
+                  </div>
+
+                  <button style="padding: 10px;color: #fff;background-color: green;margin-top: 15px;">买入</button>
+
+                </div>
+
+                <!-- 右侧的块 -->
+                <div style="padding:10px;display: flex;flex-direction: column;flex-grow: 1;">
+                  <!-- 第一行 -->
+                  <div style="display: flex;justify-content: space-between;padding: 10px 0;">
+                    <span>卖出BTC</span>
+                    <p>
+                      <span>BTC</span>
+                      <span>可用:0.00000000</span>
+                      <span>互转图标</span>
+                    </p>
+                  </div>
+                  <!-- 第二行 -->
+                  <div style="background-color: rgb(45, 52, 66);border-radius: 4px;border: 1px solid rgb(45, 52, 66);margin-top: 8px;padding: 5px;display: flex;justify-content: space-between;"> 
+                    <span>卖价</span>
+                    <input style="width:150px" placeholder="10154.7000"/>
+                    <span>USDT</span>
+                  </div>
+                  <!-- 第三行 -->
+                  <div style="padding:4px 68px">
+                    ≈ 72716.92CNY
+                  </div>
+                  <!-- 第四行 -->
+                  <div style="background-color: rgb(45, 52, 66);border-radius: 4px;border: 1px solid rgb(45, 52, 66);margin-top: 8px;padding: 5px;display: flex;justify-content: space-between;"> 
+                    <span>数量</span>
+                    <input style="width:150px" placeholder="0.0000"/>
+                    <span>BTC</span>
+                  </div>
+
+                  <!-- 第五行 -->
+                  <div style="padding: 2px 37px;display: flex;justify-content: space-between;border: 1px dashed #000;margin-top: 8px;">
+                    <span>25%</span>
+                    <span>50%</span>
+                    <span>75%</span>
+                    <span>100%</span>
+                  </div>
+
+                  <!-- 第六行 -->
+                  <div style="background-color: rgb(45, 52, 66);border-radius: 4px;border: 1px solid rgb(45, 52, 66);margin-top: 8px;padding: 5px;display: flex;justify-content: space-between;"> 
+                    <span>金额</span>
+                    <input style="width:150px" placeholder="10154.7000"/>
+                    <span>USDT</span>
+                  </div>
+
+                  <button style="padding: 10px;color: #fff;background-color: red;margin-top: 15px;">卖出</button>
+
+                </div>
+
+              </div>
+            </div>
+          </div>
+
+          <!-- 右边的块 -->
+          <div style="flex-basis: 280px;border-radius: 4px;background-color: rgb(33, 39, 51);border: 1px solid rgb(33, 39, 51);margin-left:8px">
+             <div style="text-align:center;padding:10px;background-color: rgb(45, 52, 66);">最新成交</div>
+             <div style="padding: 7px;display: flex;justify-content: space-around;">
+               <span style="padding: 5px;color: #3b85f5;font-weight: 600;border-bottom: 2px solid #3b85f5;">市场</span>
+               <span style="padding: 5px;">我的</span>
+             </div>
+             <table class="bzjy_table" border="0" cellpadding="0" cellspacing="0" width="100%" style="padding: 6px;">
+               <tbody>
+                 <tr>
+                    <td>10224.0000</td>
+                    <td>0.2930</td>
+                    <td>15:32:35</td>
+                 </tr>
+                 <tr>
+                    <td>10224.0000</td>
+                    <td>0.2930</td>
+                    <td>15:32:35</td>
+                 </tr>
+                 <tr>
+                    <td>10224.0000</td>
+                    <td>0.2930</td>
+                    <td>15:32:35</td>
+                 </tr>
+                 <tr>
+                    <td>10224.0000</td>
+                    <td>0.2930</td>
+                    <td>15:32:35</td>
+                 </tr>
+                 <tr>
+                    <td>10224.0000</td>
+                    <td>0.2930</td>
+                    <td>15:32:35</td>
+                 </tr>
+                 <tr>
+                    <td>10224.0000</td>
+                    <td>0.2930</td>
+                    <td>15:32:35</td>
+                 </tr>
+                 <tr>
+                    <td>10224.0000</td>
+                    <td>0.2930</td>
+                    <td>15:32:35</td>
+                 </tr>
+                 <tr>
+                    <td>10224.0000</td>
+                    <td>0.2930</td>
+                    <td>15:32:35</td>
+                 </tr>
+                 <tr>
+                    <td>10224.0000</td>
+                    <td>0.2930</td>
+                    <td>15:32:35</td>
+                 </tr>
+                 <tr>
+                    <td>10224.0000</td>
+                    <td>0.2930</td>
+                    <td>15:32:35</td>
+                 </tr>
+                 <tr>
+                    <td>10224.0000</td>
+                    <td>0.2930</td>
+                    <td>15:32:35</td>
+                 </tr>
+                 <tr>
+                    <td>10224.0000</td>
+                    <td>0.2930</td>
+                    <td>15:32:35</td>
+                 </tr>
+                 <tr>
+                    <td>10224.0000</td>
+                    <td>0.2930</td>
+                    <td>15:32:35</td>
+                 </tr>
+                 <tr>
+                    <td>10224.0000</td>
+                    <td>0.2930</td>
+                    <td>15:32:35</td>
+                 </tr>
+                 <tr>
+                    <td>10224.0000</td>
+                    <td>0.2930</td>
+                    <td>15:32:35</td>
+                 </tr>
+                 <tr>
+                    <td>10224.0000</td>
+                    <td>0.2930</td>
+                    <td>15:32:35</td>
+                 </tr>
+                 <tr>
+                    <td>10224.0000</td>
+                    <td>0.2930</td>
+                    <td>15:32:35</td>
+                 </tr>
+                 <tr>
+                    <td>10224.0000</td>
+                    <td>0.2930</td>
+                    <td>15:32:35</td>
+                 </tr>
+                 <tr>
+                    <td>10224.0000</td>
+                    <td>0.2930</td>
+                    <td>15:32:35</td>
+                 </tr>
+                 <tr>
+                    <td>10224.0000</td>
+                    <td>0.2930</td>
+                    <td>15:32:35</td>
+                 </tr>
+                 <tr>
+                    <td>10224.0000</td>
+                    <td>0.2930</td>
+                    <td>15:32:35</td>
+                 </tr>
+                 <tr>
+                    <td>10224.0000</td>
+                    <td>0.2930</td>
+                    <td>15:32:35</td>
+                 </tr>
+                 <tr>
+                    <td>10224.0000</td>
+                    <td>0.2930</td>
+                    <td>15:32:35</td>
+                 </tr>
+                 <tr>
+                    <td>10224.0000</td>
+                    <td>0.2930</td>
+                    <td>15:32:35</td>
+                 </tr>
+                 <tr>
+                    <td>10224.0000</td>
+                    <td>0.2930</td>
+                    <td>15:32:35</td>
+                 </tr>
+                 <tr>
+                    <td>10224.0000</td>
+                    <td>0.2930</td>
+                    <td>15:32:35</td>
+                 </tr>
+                 <tr>
+                    <td>10224.0000</td>
+                    <td>0.2930</td>
+                    <td>15:32:35</td>
+                 </tr>
+                 <tr>
+                    <td>10224.0000</td>
+                    <td>0.2930</td>
+                    <td>15:32:35</td>
+                 </tr>
+                 <tr>
+                    <td>10224.0000</td>
+                    <td>0.2930</td>
+                    <td>15:32:35</td>
+                 </tr>
+                 <tr>
+                    <td>10224.0000</td>
+                    <td>0.2930</td>
+                    <td>15:32:35</td>
+                 </tr>
+                 <tr>
+                    <td>10224.0000</td>
+                    <td>0.2930</td>
+                    <td>15:32:35</td>
+                 </tr>
+                 <tr>
+                    <td>10224.0000</td>
+                    <td>0.2930</td>
+                    <td>15:32:35</td>
+                 </tr>
+                 <tr>
+                    <td>10224.0000</td>
+                    <td>0.2930</td>
+                    <td>15:32:35</td>
+                 </tr>
+                 <tr>
+                    <td>10224.0000</td>
+                    <td>0.2930</td>
+                    <td>15:32:35</td>
+                 </tr>
+               </tbody>
+             </table> 
+          </div>
+        </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BibContent",
+  data() {
+    return {
+    };
+  },
+  computed: {
+  },
+  created() {
+    this.getData();
+  },
+  methods: {
+    getData() {
+    },
+   //  左侧的3个tab切换
+    filterLeft(index){
+
+    }
+  },
+  components: {
+  }
+};
+</script>
+
+<style scoped>
+
+.BibContent {
+  width: 100%;
+  overflow: auto;
+  box-sizing: border-box;
+}
+
+.bzjy_table th{
+  text-align: center;
+  padding: 1px 2px;
+}
+
+.bzjy_table td{
+  text-align: center;
+  padding: 1px 2px;
+}
+
+</style>
